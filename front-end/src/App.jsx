8329667725031f82876c2157fb6770b79e9b5416
yjein <yjein4220@naver.com>
2023-08-23 05:37:27 +0900

    js -> jsx로 파일명 변경

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from './components/atoms/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world!</h1>
+      <Button>test</Button>
     </div>
   );
 }

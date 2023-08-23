5bc53c376c33d88fa997d9e8b10939cb4bdc2b33
yjein <yjein4220@naver.com>
2023-08-24 00:59:23 +0900

    Main 템플릿 생성

---
+++ front-end/src/components/templates/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const mainStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "2rem",
+    backgroundColor: "#999999",
+    // minHeight: "calc(100vh - 30vh)", 
+};
+
+const Main = (props) => {
+    const { children } = props;
+    return <main style={mainStyle}>{children}</main>;
+};
+
+export default Main;

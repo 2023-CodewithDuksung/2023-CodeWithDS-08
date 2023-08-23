caf3d428edf2aef5c2e2e4d51be2b0f2179ece92
yjein <yjein4220@naver.com>
2023-08-24 01:00:20 +0900

    Button 스타일 수정

--- front-end/src/components/atoms/Button.jsx
+++ front-end/src/components/atoms/Button.jsx
@@ -1,25 +1,27 @@
 import React from "react"
 
-const buttonStyle = {
-//   padding: "0.5rem",
-//   width: "1.5rem",
-  height: "1.5rem",
-  background: "none",
-//   border: "none", 
-  borderRadius: "0.5rem",
-}
+const buttonStyle = (width, bgcolor, color) => ({
+    padding: "1rem",
+    width: width,
+    height: "1.5rem",
+    border: "none", 
+    borderRadius: "1rem",
+    fontSize: "1rem",
+    color: color,
+    backgroundColor: bgcolor,
+})
 
 const Button = (props) => {
-  const { children } = props
+    const { children, width, bgcolor, color } = props
 
-  return (
-    <button
-      style={buttonStyle}
-    //   onClick={() => setToggleState(!toggleState)}
-    >
-      {children}
-    </button>
-  )
+    return (
+        <button
+        style={buttonStyle(width, bgcolor, color)}
+        //   onClick={() => setToggleState(!toggleState)}
+        >
+        {children}
+        </button>
+    )
 }
 
 export default Button
